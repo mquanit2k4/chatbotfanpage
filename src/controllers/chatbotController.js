bed1bb666c401bfc292d794b566de03673013c4b
Vũ Minh Quân <Quan.VM225910@sis.hust.edu.vn>
2024-09-06 17:52:40 +0700

    update

--- src/controllers/chatbotController.js
+++ src/controllers/chatbotController.js
@@ -66,49 +66,50 @@ let getWebhook = (req, res) => {
 };
 
 function handleMessage(sender_psid, received_message) {
-
   let response;
 
   // Check if the message contains text
-  if (received_message.text) {    
-
+  if (received_message.text) {
     // Create the payload for a basic text message
     response = {
-      "text": `Bạn đã gửi tin nhắn là: "${received_message.text}". Bây giờ gửi cho mình một cái ảnh!`
-    }
-  }  
-  
+      text: `Bạn đã gửi tin nhắn là: "${received_message.text}". Bây giờ gửi cho mình một cái ảnh!`,
+    };
+  }
+
   // Sends the response message
-  callSendAPI(sender_psid, response);    
+  callSendAPI(sender_psid, response);
 }
 
 // Handles messaging_postbacks events
-function handlePostback(sender_psid, received_postback) {
-  // Construct the message body
-  let request_body = {
-    "recipient": {
-      "id": sender_psid
-    },
-    "message": response
-  }
-
-  // Send the HTTP request to the Messenger Platform
-  request({
-    "uri": "https://graph.facebook.com/v2.6/me/messages",
-    "qs": { "access_token": PAGE_ACCESS_TOKEN },
-    "method": "POST",
-    "json": request_body
-  }, (err, res, body) => {
-    if (!err) {
-      console.log('message sent!')
-    } else {
-      console.error("Unable to send message:" + err);
-    }
-  }); 
-}
+function handlePostback(sender_psid, received_postback) {}
 
 // Sends response messages via the Send API
-function callSendAPI(sender_psid, response) {}
+function callSendAPI(sender_psid, response) {
+  // Construct the message body
+  let request_body = {
+    recipient: {
+      id: sender_psid,
+    },
+    message: response,
+  };
+
+  // Send the HTTP request to the Messenger Platform
+  request(
+    {
+      uri: "https://graph.facebook.com/v2.6/me/messages",
+      qs: { access_token: PAGE_ACCESS_TOKEN },
+      method: "POST",
+      json: request_body,
+    },
+    (err, res, body) => {
+      if (!err) {
+        console.log("message sent!");
+      } else {
+        console.error("Unable to send message:" + err);
+      }
+    }
+  );
+}
 export default {
   getHomepage,
   getWebhook,
