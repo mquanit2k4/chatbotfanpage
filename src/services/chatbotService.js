1190c337063688146dd168b91d4070649f68b0b2
Vũ Minh Quân <Quan.VM225910@sis.hust.edu.vn>
2024-09-12 17:51:10 +0700

    update

--- src/services/chatbotService.js
+++ src/services/chatbotService.js
@@ -2,6 +2,9 @@ import request from "request";
 import dotenv from "dotenv";
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 const IMAGE_GET_STARTED = `https://bit.ly/imagestarted`;
+const sleep = (ms) => {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  };
 dotenv.config();
 let callSendAPI = (sender_psid, response) => {
   // Construct the message body
@@ -111,26 +114,34 @@ let handleSendInfoCourseSingle = (sender_psid) => {
         text: `Học phí của khóa hiện tại là 200k nhưng nếu em muốn nhận voucher giảm giá chỉ còn 150K thì làm theo các bước like và share trong post này https://www.facebook.com/dobknhe/posts/491015820`,
       };
 
-      await callSendAPI(sender_psid, response1);
 
       let response2 = {
         text: `Khi tham gia khoá học e không chỉ được học các phương pháp giải nhanh + CASIO các dạng bài trong đề thi THPTQG mà còn được ôn luyện các đề thi thử của Trường, Sở qua các buổi LIVE hằng tuần để phục vụ cho kì thi THPTQG 2025 luôn nha. Khoá này kéo dài đến lúc cb thi xong kì thi THPT luôn nhé 🥰`,
       };
-      await callSendAPI(sender_psid, response2);
 
       let response3 = {
         text: `Chào em, admin xin giới thiệu tổng quan về khóa CASIO VD-VDC THPTQG 2K7 đến em: Khóa học sẽ cung cấp cho các bạn tất cả các phương pháp bấm máy + cách tư duy bản chất để làm các dạng toán từ mức VD (vận dụng, mức 8+ THPTQG) đến VDC (vận dụng cao, mức 9+ THPTQG) ở tất cả các chương trong chương trình Toán 12 (theo chuẩn chương trình mới). Có VIDEO + FILE hướng dẫn chi tiết phương pháp và bài tập kèm theo để các bạn luyện tập và nắm vững phương pháp làm bài. Để biết thêm thông tin em có thể xem ở bài viết https://www.facebook.com/dobknhe/posts/491015820355603 này nha 💕`,
       };
-      await callSendAPI(sender_psid, response3);
 
       let response4 = {
         text: `Song song với việc học các phương pháp qua VIDEO và học LIVE hằng tuần trong group kín. Các bạn còn được giải đáp thắc mắc đối với các bài tập có thể sử dụng CASIO được thông qua việc tham gia box chat có sự hỗ trợ bởi chính admin và các anh chị CTV 2k5, 2k6 có thành tích xuất sắc ✨`,
       };
-      await callSendAPI(sender_psid, response4);
 
       //   let response5 = getInfoCourseTemplate();
       //   // Send text message
       //   await callSendAPI(sender_psid, response);
+      await callSendAPI(sender_psid, response3);
+      console.log('Sent response1');
+      await sleep(1000); 
+      await callSendAPI(sender_psid, response2);
+      console.log('Sent response1');
+      await sleep(1000); 
+      await callSendAPI(sender_psid, response4);
+      console.log('Sent response1');
+      await sleep(1000); 
+      await callSendAPI(sender_psid, response1);
+      console.log('Sent response1');
+      await sleep(1000); 
 
       resolve("done");
     } catch (e) {
