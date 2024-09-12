66011b34c357ec1f84d329dd08f149b4cc2f78a0
Vũ Minh Quân <Quan.VM225910@sis.hust.edu.vn>
2024-09-12 16:42:44 +0700

    update

--- src/services/chatbotService.js
+++ src/services/chatbotService.js
@@ -1,15 +1,15 @@
 import request from "request";
 import dotenv from "dotenv";
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
-
+const IMAGE_GET_STARTED = `https://bit.ly/imagestarted`;
 dotenv.config();
 let callSendAPI = (sender_psid, response) => {
-// Construct the message body
-let request_body = {
+  // Construct the message body
+  let request_body = {
     recipient: {
       id: sender_psid,
     },
-    message: response,  
+    message: response,
   };
 
   // Send the HTTP request to the Messenger Platform
@@ -27,47 +27,83 @@ let request_body = {
         console.error("Unable to send message:" + err);
       }
     }
-  );   
-}
+  );
+};
 
 let getUserName = (sender_psid) => {
-    return new Promise((resolve, reject) => {
-      // Send the HTTP request to the Messenger Platform
-     request(
-        {
-          uri: `https://graph.facebook.com/${sender_psid}?fields=first_name,last_name,profile_pic&access_token=${PAGE_ACCESS_TOKEN}`,
-          method: "GET",
-        },
-        (err, res, body) => {
-          console.log(body);
-          if (!err) {
-            body = JSON.parse(body);
-            let userName = `${body.last_name} ${body.first_name} `;
-            resolve (userName);
-          } else {
-            console.error("Unable to send message:" + err);
-            reject(err);
-          }
+  return new Promise((resolve, reject) => {
+    // Send the HTTP request to the Messenger Platform
+    request(
+      {
+        uri: `https://graph.facebook.com/${sender_psid}?fields=first_name,last_name,profile_pic&access_token=${PAGE_ACCESS_TOKEN}`,
+        method: "GET",
+      },
+      (err, res, body) => {
+        console.log(body);
+        if (!err) {
+          body = JSON.parse(body);
+          let userName = `${body.last_name} ${body.first_name}`;
+          resolve(userName);
+        } else {
+          console.error("Unable to send message:" + err);
+          reject(err);
         }
-      );   
-});
-}
+      }
+    );
+  });
+};
 let handleGerStarted = (sender_psid) => {
-    return new Promise(async (resolve, reject) => {
-        try {
-            let userName = await getUserName(sender_psid);
-            let response = {
-                text: `Chào ${userName}, em vui lòng nhắn 'casio' để được tư vấn chi tiết về khóa học nhé <3`,
-              };
-            await callSendAPI(sender_psid, response);
-            resolve('done');
-        } catch (e) {   
-            reject(e);
-        }
-});
-}
+  return new Promise(async (resolve, reject) => {
+    try {
+      let userName = await getUserName(sender_psid);
+      let response1 = {
+        text: `Chào ${userName}, em vui lòng nhắn 'casio' để được tư vấn chi tiết về khóa học nhé <3`,
+      };
+
+      let response2 = sendGetStartedTemplate();
+      // Send text message
+      await callSendAPI(sender_psid, response);
+
+    // Send template message
+      resolve("done");
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+let sendGetStartedTemplate = () => {
+  let response = {
+    attachment: {
+      type: "template",
+      payload: {
+        template_type: "generic",
+        elements: [
+          {
+            title: "KHÓA HỌC CASIO VD-VDC 9+ 2K7",
+            subtitle: "Dưới đây là 2 khóa học CASIO của page",
+            image_url: IMAGE_GET_STARTED,
+            buttons: [
+              {
+                type: "postback",
+                title: "KHÓA CASIO VD-VDC THPTQG 2K7",
+                payload: "THPT",
+              },
+              {
+                type: "postback",
+                title: "KHÓA CASIO VD-VDC THPTQG + HSA 2K7",
+                payload: "HSA",
+              },
+            ],
+          },
+        ],
+      },
+    },
+  };
+  return response;
+};
 
 export default {
-    handleGerStarted,
-    callSendAPI,
+  handleGerStarted,
+  callSendAPI,
 };
